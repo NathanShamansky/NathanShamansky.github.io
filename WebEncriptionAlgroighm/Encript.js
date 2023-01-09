231699c1ac7fabd14c75fb6ab5d79603f2a4e637
NathanShamansky <120989364+NathanShamansky@users.noreply.github.com>
2023-01-08 21:52:22 -0800

    Update Encript.js

--- WebEncriptionAlgroighm/Encript.js
+++ WebEncriptionAlgroighm/Encript.js
@@ -1,23 +1,11 @@
 function encrypt(text, key) {
-    // Create an empty string to store the encrypted text
-    let encryptedText = "";
+  // Convert text and key to arrays of ASCII values
+  var textArr = text.split('').map(char => char.charCodeAt(0));
+  var keyArr = key.split('').map(char => char.charCodeAt(0));
   
-    // Loop through each character in the text
-    for (let i = 0; i < text.length; i++) {
-      // Get the ASCII code for the character
-      let asciiCode = text.charCodeAt(i);
+  // Encrypt each character in the text by adding its ASCII value to the corresponding key character's ASCII value
+  var encryptedArr = textArr.map((char, index) => char + keyArr[index % keyArr.length]);
   
-      // Add the key to the ASCII code and get the modulus of the result
-      // with 256. This ensures that the resulting ASCII code is always between
-      // 0 and 255
-      let encryptedCode = (asciiCode + Number(key)) % 256;
-  
-      // Convert the encrypted ASCII code back to a character and add it to
-      // the encrypted text
-      let encryptedChar = String.fromCharCode(encryptedCode);
-      encryptedText += encryptedChar;
-    }
-  
-    // Return the encrypted text
-    return encryptedText;
+  // Convert encrypted array back to string and return it
+  return encryptedArr.map(char => String.fromCharCode(char)).join('');
   }
